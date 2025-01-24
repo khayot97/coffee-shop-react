0eb6f9a9332875da147f1d7e22562bb0ca45ead1
Khayotbek <khayotbekabdurashidov97gmail.com>
2025-01-24 16:31:12 +0900

    feat: Publish orderPage screen component

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -7,6 +7,7 @@ import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
 import "../../../css/home.css"
 
+
 export default function HomePage() {
     return <div className={"homepage"}>
       <Statistics />
