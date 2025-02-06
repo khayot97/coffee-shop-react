0a52c3e05288cb79ef45dcc9227c752f4643d195
Khayotbek <khayotbekabdurashidov97gmail.com>
2025-02-06 22:55:32 +0900

    feat: Develop slice and selector usage

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -7,25 +7,39 @@ import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
 import "../../../css/home.css"
 
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { setPopularDishes } from "./slice";
+import { retrievePopularDishes} from "./selector";
+import { Product } from "../../../lib/types/product";
+
+/** REDUX SLICE & SELECTOR **/
+
+const actionDispatch = (dispatch: Dispatch) => ({
+  setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+});
+
+const popularDishesRetriever = createSelector(
+  retrievePopularDishes,
+  (popularDishes) => ({ popularDishes })
+);
+
 
 export default function HomePage() {
-// Selector: Store => Data
+ const { setPopularDishes } = actionDispatch(useDispatch());
+ const { popularDishes } = useSelector(popularDishesRetriever);
 
-useEffect(() => {
-  // Backend server data request => Data
-  // slice: Data => Store
-}, []);
+useEffect(() => {}, []);
 
-
-
-
-
-    return <div className={"homepage"}>
+    return (
+    <div className={"homepage"}>
       <Statistics />
       <PopularDishes />
       <NewDishes />
       <Advertisement />
       <ActiveUsers />
       <Events />
-    </div>;
+    </div>
+  );  
 }
