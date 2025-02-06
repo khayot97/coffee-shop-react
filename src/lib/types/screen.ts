ba63965b4b1fec4f0e0eef0989ceaac0fe537dd5
Khayotbek <khayotbekabdurashidov97gmail.com>
2025-02-06 21:47:24 +0900

    feat: Develop AppRootState and type integrations

---
+++ src/lib/types/screen.ts
@@ -0,0 +1,20 @@
+
+import { Member } from "./member";
+import { Product } from "./product";
+
+/** REACT APP STATE  */
+export interface AppRootState {
+    homePage: HomePageState;
+}
+
+/** HOMEPAGE  */
+export interface HomePageState {
+    popularDishes: Product[];
+    newDishes: Product[];
+    topUsers: Member[];
+}
+
+/** PRODUCTPAGE  */
+
+
+/** ORDERS PAGE   */
