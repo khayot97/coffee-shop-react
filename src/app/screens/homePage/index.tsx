a3f006ae5a6bf151b1534581c05563885bc79e59
Khayotbek <khayotbekabdurashidov97gmail.com>
2025-02-07 20:35:59 +0900

    feat: Create config & env variables

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -30,7 +30,8 @@ export default function HomePage() {
  const { setPopularDishes } = actionDispatch(useDispatch());
  const { popularDishes } = useSelector(popularDishesRetriever);
 
-useEffect(() => {}, []);
+
+  useEffect(() => {}, []);
 
     return (
     <div className={"homepage"}>
