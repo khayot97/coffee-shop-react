9af9e50aeaf71a35b1d69dbcf74d1f4fcd6e747f
Khayotbek <khayotbekabdurashidov97gmail.com>
2025-02-11 21:08:56 +0900

    fix: Retreive data and modify products component

--- src/app/screens/productsPage/Products.tsx
+++ src/app/screens/productsPage/Products.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box, Button, Container, Stack } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
@@ -8,13 +8,15 @@ import Pagination from "@mui/material/Pagination";
 import PaginationItem from "@mui/material/PaginationItem";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-
 import { useDispatch, useSelector} from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
 import { setProducts } from "./slice";
 import { createSelector } from "reselect";
 import { retrieveProduct } from "./selector";
 import { Product } from "../../../lib/types/product";
+import ProductService from "../../services/ProductService";
+import { ProductCollection } from "../../../lib/enums/product.enum";
+import { serverApi } from "../../../lib/config";
 
 const actionDispatch = (dispatch: Dispatch) => ({
   setProducts: (data: Product[]) => dispatch(setProducts(data)),
@@ -24,18 +26,23 @@ const productsRetriver = createSelector(retrieveProduct, (products) => ({
     products,
 }));
 
-const products = [
-    { productName: "Cutlet", imagePath: "/img/cutlet.webp" },
-    { productName: "Kebab", imagePath: "/img/kebab-fresh.webp" },
-    { productName: "Kebab", imagePath: "/img/kebab.webp" },
-    { productName: "Lavash", imagePath: "/img/lavash.webp" },
-    { productName: "Lavash", imagePath: "/img/lavash.webp" },
-    { productName: "Cutlet", imagePath: "/img/cutlet.webp" },
-    { productName: "Kebab", imagePath: "/img/kebab.webp" },
-    { productName: "Kebab", imagePath: "/img/kebab.webp" },
-];
-
 export default function Products() {
+    const { setProducts } = actionDispatch(useDispatch());
+    const { products } = useSelector(productsRetriver);
+
+    useEffect(() => {
+        const product = new ProductService();
+        product
+        .getProducts({
+            page: 1,
+            limit: 8,
+            order: "createdAt",
+            productCollection: ProductCollection.DISH,
+            search: "",
+        })
+        .then((data) => setProducts(data))
+        .catch((err) => console.log(err));
+    }, []);
     return (
             <div className={"products"}>
                 <div className={"body"} />
@@ -109,69 +116,74 @@ export default function Products() {
                     </Stack>
                     <Stack className={"product-wrapper"}>
                         {products.length !== 0 ? (
-                        products.map((product, index) => {
+                        products.map((product: Product) => {
+                            const imagePath = `${serverApi}/${product.productImages[0]}`;
+                            const sizeVolume = 
+                                product.productCollection === ProductCollection.DRINK
+                                    ? product.productVolume + " litre"
+                                    : product.productSize + " size";
                             return (
-                
-                            <Stack key={index} className={"product-card"}>
-                                <Stack
-                                className={"product-img"}
-                                sx={{ backgroundImage: `url(${product.imagePath})` }}
-                                >
-                                <div className={"product-sale"}>Normal Size</div>
-                                <Button className={"shop-btn"}>
-                                    <img
-                                    src={"/icons/shopping-cart.svg"}
-                                    style={{ display: "flex" }}
-                                    />
-                                </Button>
-                                <Button className={"view-btn"} sx={{ right: "36px" }}>
-                                    <Badge
-                                        badgeContent={20}
-                                        color="secondary"
+                                <Stack key={product._id} className={"product-card"}>
+                                    <Stack
+                                    className={"product-img"}
+                                    sx={{ backgroundImage: `url(${imagePath})` }}
                                     >
-                                    <RemoveRedEyeIcon
-                                         sx={{
-                                            color: 0 ? "gray" : "white",
-                                         }}
-                                    />
-                                    </Badge>
-                                </Button>
+                                    <div className={"product-sale"}>{sizeVolume}</div>
+                                    <Button className={"shop-btn"}>
+                                        <img
+                                        src={"/icons/shopping-cart.svg"}
+                                        style={{ display: "flex" }}
+                                        />
+                                    </Button>
+                                    <Button className={"view-btn"} sx={{ right: "36px" }}>
+                                        <Badge
+                                            badgeContent={product.productViews}
+                                            color="secondary"
+                                        >
+                                        <RemoveRedEyeIcon
+                                            sx={{
+                                                color: 
+                                                product.productViews === 0 ? "gray" : "white",
+                                            }}
+                                        />
+                                        </Badge>
+                                    </Button>
+                                    </Stack>
+                                    <Box className={"product-desc"}>
+                                    <span className={"product-title"}>
+                                        {product.productName}
+                                    </span>
+                                    <div className={"product-desc"}>
+                                        <MonetizationOnIcon />
+                                        {product.productPrice}
+                                    </div>
+                                    </Box>
                                 </Stack>
-                                <Box className={"product-desc"}>
-                                <span className={"product-title"}>
-                                    {product.productName}
-                                </span>
-                                <div className={"product-desc"}>
-                                    <MonetizationOnIcon />
-                                    {12}
-                                </div>
-                                </Box>
-                            </Stack>
-                            );
-                        })
-                        ) : (
-                        <Box className="no-data">Products are not available!</Box>
-                        )}
-                    </Stack>
-                    </Stack>
+                                );
+                            })
+                            ) : (
+                            <Box className="no-data">Products are not available!</Box>
+                            )}
+                        </Stack>
+                        </Stack>
 
-                    <Stack className={"pagination-section"}>
-                    <Pagination
-                        count={3}
-                        page={1}
-                        renderItem={(item) => (
-                            <PaginationItem
-                                components={{
-                                previous: ArrowBackIcon,
-                                next: ArrowForwardIcon,
-                                }}
-                                {...item}
-                                color={"secondary"}
-                            />
-                        )}
-                    />
+                        <Stack className={"pagination-section"}>
+                        <Pagination
+                            count={3}
+                            page={1}
+                            renderItem={(item) => (
+                                <PaginationItem
+                                    components={{
+                                    previous: ArrowBackIcon,
+                                    next: ArrowForwardIcon,
+                                    }}
+                                    {...item}
+                                    color={"secondary"}
+                                />
+                            )}
+                        />
+                        </Stack>
                     </Stack>
-                </Stack>
                 </Container>
                 <div className={"brands-logo"}>
                 <Container className={"family-brands"}>
@@ -210,3 +222,7 @@ export default function Products() {
         </div>
     );
 };
+// function actionDispatch(arg0: (action: T) => T): { setProducts: any; } {
+//     throw new Error("Function not implemented.");
+// }
+
